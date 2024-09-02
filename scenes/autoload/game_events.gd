794567e5498578c7dc80cb62fdc7f6c72e609ad7
Md Azizul Hakim <mdazizulhakim.cse@gmail.com>
2024-09-02 19:49:59 +0600

    sword rate ability upgrade

--- scenes/autoload/game_events.gd
+++ scenes/autoload/game_events.gd
@@ -1,7 +1,11 @@
 extends Node
 
 signal experience_vial_collection(number: float)
+signal ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrade: Dictionary)
 
 func emit_experience_vile_Collected(number: float):
 	experience_vial_collection.emit(number)
+	
+func emit_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrade: Dictionary):
+	ability_upgrade_added.emit(upgrade,current_upgrade)
 	
