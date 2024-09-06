2941bf82ee4094c65d3cc002ff703e02993a6d4c
Md Azizul Hakim <mdazizulhakim.cse@gmail.com>
2024-09-07 00:13:44 +0600

    player hp bar and take damage

--- scenes/component/health_component.gd
+++ scenes/component/health_component.gd
@@ -2,20 +2,27 @@ extends Node
 class_name HealthComponent
 
 signal died
+signal health_changed
 
 @export var max_health: float = 10
-var current_helath
+var current_health
 
 
 func _ready():
-	current_helath = max_health
+	current_health = max_health
 
 func damage(damageReceived: float):
-	current_helath = max(current_helath - damageReceived, 0)
+	current_health = max(current_health - damageReceived, 0)
+	health_changed.emit()
 	Callable(death).call_deferred()
+	
+	
+func get_health_percent():
+	if max_health <= 0 : return 0
+	return min(current_health / max_health, 1)
 	
 		
 func death():
-	if current_helath == 0:
+	if current_health == 0:
 		died.emit()
 		owner.queue_free()
