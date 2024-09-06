8bc4a1f9313a3cda6bce0e31b5cea5bb98602e6e
Md Azizul Hakim <mdazizulhakim.cse@gmail.com>
2024-09-07 01:06:17 +0600

    defeat screen and end screen

--- scenes/manager/arena_timer_manager.gd
+++ scenes/manager/arena_timer_manager.gd
@@ -1,5 +1,5 @@
 extends Node
-@export var victory_screen_scene: PackedScene
+@export var end_screen_scene: PackedScene
 
 @onready var timer = $Timer
 
@@ -10,5 +10,5 @@ func get_time_elapsed():
 	return timer.wait_time - timer.time_left
 	
 func on_timer_timeout():
-	var victory_screen_instance = victory_screen_scene.instantiate()
-	add_child(victory_screen_instance)
+	var end_screen_instance = end_screen_scene.instantiate()
+	add_child(end_screen_instance)
